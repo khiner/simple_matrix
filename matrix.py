86d32ddf2163a7c2562726956612c3887e4b5bab
Karl Hiner <karl.hiner@gmail.com>
2016-04-09 14:05:09 -0700

    Proper MatrixXMatrix multiplication, not just special case of NxM * Mx1 (handling NxM * MxO)

--- matrix.py
+++ matrix.py
@@ -4,7 +4,10 @@ class Matrix:
   def __init__(self, n_rows_or_list = 1, n_columns = 1):
     if isinstance(n_rows_or_list, list):
       self.n_rows = len(n_rows_or_list)
-      self.n_columns = 1
+      if len(n_rows_or_list) > 0 and isinstance(n_rows_or_list[0], list):
+        self.n_columns = len(n_rows_or_list[0])
+      else:
+        self.n_columns = 1
     else:
       if isinstance(n_rows_or_list, (int, long)):
         self.n_rows = n_rows_or_list
@@ -13,10 +16,14 @@ class Matrix:
         print 'Don\'t know what to do with a ' + type(n_rows_or_list) + ' constructor. Defaulting to an empty 1-d matrix'
         self.n_rows = self.n_columns = 1
 
-    self.values = [[0 for i in range(n_columns)] for j in range(self.n_rows)]
+    self.values = [[0 for i in range(self.n_columns)] for j in range(self.n_rows)]
     if isinstance(n_rows_or_list, list):
       for row_index, row_value in enumerate(n_rows_or_list):
-        self.values[row_index][0] = row_value
+        if isinstance(row_value, list):
+          for column_index, column_value in enumerate(row_value):
+            self.values[row_index][column_index] = column_value
+        else:
+          self.values[row_index][0] = row_value
 
   def __add__(self, other):
     if isinstance(other, Matrix):
@@ -42,9 +49,9 @@ class Matrix:
     string = ''
     for row in self.values:
       string += '['
-      for columnIndex, value in enumerate(row):
+      for column_index, value in enumerate(row):
         string += str(value)
-        if columnIndex != self.n_columns - 1:
+        if column_index != self.n_columns - 1:
           string += ' '
       string += "]\n"
 
@@ -59,9 +66,9 @@ class Matrix:
 
   def __plus_matrix(self, other):
     if other.n_rows == self.n_rows and other.n_columns == self.n_columns:
-      for rowIndex, row in enumerate(self.values):
-        for columnIndex, value in enumerate(row):
-          self.values[rowIndex][columnIndex] += other.values[rowIndex][columnIndex]
+      for row_index, row in enumerate(self.values):
+        for column_index, value in enumerate(row):
+          self.values[row_index][column_index] += other.values[row_index][column_index]
     else:
       print 'Cannot add a matrix by another with different dimensionality.'
 
@@ -75,21 +82,18 @@ class Matrix:
     return self
 
   def __times_matrix(self, other):
-    if other.n_rows == self.n_rows and other.n_columns == self.n_columns:
-      for rowIndex, row in enumerate(self.values):
-        for columnIndex, value in enumerate(row):
-          self.values[rowIndex][columnIndex] *= other.values[rowIndex][columnIndex]
-    elif other.n_rows == self.n_columns and other.n_columns == 1:
-      result_vector = Matrix(self.n_rows)
-      for rowIndex, row in enumerate(self.values):
-        result_vector.values[rowIndex][0] = 0
-        for columnIndex, value in enumerate(row):
-          result_vector.values[rowIndex][0] += value * other.values[columnIndex][0]
-      return result_vector
+    if other.n_rows == self.n_columns:
+      result_matrix = Matrix(self.n_rows, other.n_columns)
+      for row_index, row in enumerate(self.values):
+        for other_column_index in range(other.n_columns):
+          result_matrix.values[row_index][other_column_index] = 0
+          for column_index, value in enumerate(row):
+            result_matrix.values[row_index][other_column_index] += value * other.values[column_index][other_column_index]
+      return result_matrix
     else:
-      print 'Can only multiply a matrix by one with the same dimensionality or a vector with row length equal to this matrix\'s column length'
+      print 'Can only multiply a matrix with shape NXM by another with shape MXO'
 
     return self
 
-#matrix = (Matrix(2, 3) + 2 * 3 + (Matrix(2, 3) + 10) + 100) * [1, 1, 2]
-#print matrix.to_string()
+matrix = (Matrix(2, 3) + 2 * 3 + (Matrix(2, 3) + 10) + 100) * [[1,2], [2,3], [3,4]]
+print matrix.to_string()
