a5bf2f7886c79c05f9b7e31e9063084f23c57495
Karl Hiner <karl.hiner@gmail.com>
2016-04-10 12:41:47 -0700

    Append row/column

--- matrix.py
+++ matrix.py
@@ -58,6 +58,54 @@ class Matrix:
         string += "\n"
     return string
 
+  def append_row(self, new_row):
+    if isinstance(new_row, (int, long, float, complex)):
+      new_row = [new_row] # create a single-element list if input is number
+    elif not isinstance(new_row, list):
+      print 'Only know how to append lists or numbers to a matrix'
+      return
+
+    columns_to_pad = len(new_row) - self.n_columns
+    if columns_to_pad > 0:
+      # pad existing rows with zeros to match new dimensionality
+      for row in self.values:
+        for i in range(columns_to_pad):
+          row.append(0)
+    elif columns_to_pad < 0:
+      # pad new row with zeros to match existing dimensionality
+      for i in range(-columns_to_pad):
+        new_row.append(0)
+
+    self.values.append(new_row)
+    self.n_rows += 1
+    self.n_columns = len(new_row)
+
+    return self
+
+  def append_column(self, new_column):
+    if isinstance(new_column, (int, long, float, complex)):
+      new_column = [new_column] # create a single-element list if input is number
+    elif not isinstance(new_column, list):
+      print 'Only know how to append lists or numbers to a matrix'
+      return
+
+    rows_to_pad = len(new_column) - self.n_rows
+    if rows_to_pad > 0:
+      # pad with new 0-filled rows to match new dimensionality
+      self.values.append([0] * self.n_columns)
+    elif rows_to_pad < 0:
+      # pad new row with zeros to match existing dimensionality
+      for i in range(-rows_to_pad):
+        new_column.append(0)
+
+    for row_index, row in enumerate(self.values):
+      row.append(new_column[row_index])
+
+    self.n_columns += 1
+    self.n_rows = len(self.values)
+
+    return self
+
   def transpose(self):
     transposed_matrix = Matrix(self.n_columns, self.n_rows)
     for row_index, row_value in enumerate(self.values):
@@ -104,6 +152,8 @@ class Matrix:
 
     return self
 
+
+
 matrix = Matrix([[1,2], [2,3], [3,4]])
 print matrix
 print 'transposed'
@@ -121,3 +171,9 @@ print '*'
 print identity_matrix
 print '='
 print square_matrix * identity_matrix
+square_matrix.append_row([1,2,3,4,5,6])
+print 'not square anymore'
+print square_matrix
+square_matrix.append_column([1,2,3,4,5,6])
+print 'square again!'
+print square_matrix
